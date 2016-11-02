3ad245c7c3c775603def9faf63021e5a64d12ec3
Miguel Saldivar <miguel.saldivar22@hotmail.com>
2016-11-01 21:37:30 -0700

    lets get this show on the road

---
+++ include/cpu.h
@@ -0,0 +1,111 @@
+#ifndef CPU_H
+#define CPU_H
+
+#include <map>
+
+typedef unsiged uint8_t u8;
+typedef unsiged uint16_t u16;
+typedef unsiged uint32_t u32;
+
+class CPU
+{
+private:
+    typedef void(CPU::*FP)
+
+    // 16-bit registers
+    u16 AF; // Accumulator & Flags
+    u16 BC;
+    u16 DE;
+    u16 HL;
+    u16 SP; // Stack Pointer
+    u16 PC; // Program Counter/Pointer
+
+    // function pointer map, used to go to loaded instruction
+    // without use of switch or if-else statements
+    map<int, FP> instr;
+public:
+    void run(char *);
+
+    /*********** Instructions *************/
+
+    // 8-bit load instr
+    void ld_r_r();
+    void ld_r_n();
+    void ld_r_hl();
+    void ld_hl_r();
+    void ld_hl_n();
+    void ld_a_bc();
+    void ld_a_de();
+    void ld_a_nn();
+    void ld_bc_a();
+    void ld_de_a();
+    void ld_nn_a();
+    void ld_a_ff00();
+    void ld_ff00_a();
+    void ld_a_ff00c();
+    void ld_ff00c_a();
+
+    void ldi_hl_a();
+    void ldi_a_hl();
+    
+    void ldd_hl_a();
+    void ldd_a_hl();
+
+
+    // 16-bit load instr
+    void ld_rr_nn();
+    void ld_sp_hl();
+    void push_rr();
+    void pop_rr();
+
+    // 8-bit arithmetic/logic instr
+    void add_a_r();
+    void add_a_n();
+    void add_a_hl();
+
+    void adc_a_r();
+    void adc_a_n();
+    void adc_a_hl();
+
+    void sub_r();
+    void sub_n();
+    void sub_hl();
+
+    void sbc_a_r();
+    void sbc_a_n();
+    void sbc_a_hl();
+
+    void and_r();
+    void and_n();
+    void and_hl();
+
+    void xor_r();
+    void xor_n();
+    void xor_hl();
+
+    void or_r();
+    void or_n();
+    void or_hl();
+
+    void cp_r();
+    void cp_n();
+    void cp_hl();
+
+    void inc_r();
+    void inc_hl();
+
+    void dec_r();
+    void dec_hl();
+
+    void daa();
+    void cpl();
+
+    // 16-bit arithmetic/logical instr
+    void add_hl_rr();
+    void inc_rr();
+    void dec_rr();
+    void add_sp_dd();
+    void hl_spdd();
+}
+
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "cpu.h"
+
+int main(int argv, char **argc)
+{
+    CPU c;
+    c.run(argc[1]);
+}
